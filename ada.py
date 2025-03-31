2667eb499fb9d17f479e1bf4dd432b269b5e1255
ltomysdemello <ltomysdemello@mail.austral.edu.ar>
2025-03-31 15:31:02 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,9 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    full_name = first_name + " " + last_name
-    print (first_name.lower() + " "+ last_name.lower())
-    print (first_name.title() + " " + last_name.title())
-    print (first_name.upper() + " " + last_name.upper())
+   first_name = "AdA"
+    last_name  = "LoVeLAce"
+    full_name = f" {first_name} {last_name}"
+    print (full_name.lower())
+    print (full_name.title())
+    print (full_name.upper())
     print (f"\t{full_name.lower()}")
  
