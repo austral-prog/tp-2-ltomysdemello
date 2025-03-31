f9a3614e89b3dcc6e6892a3ef3eb2e809c8f54e2
ltomysdemello <ltomysdemello@mail.austral.edu.ar>
2025-03-31 15:01:48 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -6,4 +6,23 @@ def ada():
     print (first_name.title() + " " + last_name.title())
     print (first_name.upper() + " " + last_name.upper())
     print (f"\t {full_name.lower()}")
-ada()
+    X = "Bangladesh"
+    Y = "Barbados"
+    print (f"The result of {X} comes first in the dictionary than {Y} is {X<Y}.")
+    print (f"The result of {Y} comes first in the dictionary than {X} is {X>Y}.")
+
+    money = 100
+    expense = 23.75
+    vuelto = money - expense
+    pesos = int(vuelto)
+    centavos = int((vuelto - pesos)*100)
+
+    print ("Ingresar gasto:")
+    print (expense)
+    print ("Dinero recibido:")
+    print (money)
+    print(f"\nVuelto")
+    print (f"\nPesos:")
+    print(vuelto)
+    print("Centavos:")
+    print(centavos)
